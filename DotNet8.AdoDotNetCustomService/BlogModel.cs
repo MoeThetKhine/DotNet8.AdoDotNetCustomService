7507892e8cb4d1983276de57404e3b1a085acf27
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-29 22:04:44 +0630

    -

--- DotNet8.AdoDotNetCustomService/BlogModel.cs
+++ DotNet8.AdoDotNetCustomService/BlogModel.cs
@@ -1,10 +1,14 @@
 ﻿namespace DotNet8.AdoDotNetCustomService;
 
+#region Blog Model
+
 public class BlogModel
 {
 	public long BlogId {  get; set; }
-	public string BlogTitle { get; set; }
-	public string BlogAuthor { get; set; }
-	public string BlogContent {  get; set; }
+	public string? BlogTitle { get; set; }
+	public string? BlogAuthor { get; set; }
+	public string? BlogContent {  get; set; }
 	public bool DeleteFlag {  get; set; }
 }
+
+#endregion
