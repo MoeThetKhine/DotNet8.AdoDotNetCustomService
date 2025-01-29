5ad03f9da6460938a8871a9757494adc5b9c2543
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-29 22:03:04 +0630

    -

--- DotNet8.AdoDotNetCustomService/BlogModel.cs
+++ DotNet8.AdoDotNetCustomService/BlogModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.AdoDotNetCustomService
+﻿namespace DotNet8.AdoDotNetCustomService
 {
 	public class BlogModel
 	{
