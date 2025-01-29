86f437b33a40ffc2ef9a8fd520521528e49185a4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-29 22:03:42 +0630

    Format Code

--- DotNet8.AdoDotNetCustomService/BlogModel.cs
+++ DotNet8.AdoDotNetCustomService/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.AdoDotNetCustomService
+﻿namespace DotNet8.AdoDotNetCustomService;
+
+public class BlogModel
 {
-	public class BlogModel
-	{
-		public long BlogId {  get; set; }
-		public string BlogTitle { get; set; }
-		public string BlogAuthor { get; set; }
-		public string BlogContent {  get; set; }
-		public bool DeleteFlag {  get; set; }
-	}
+	public long BlogId {  get; set; }
+	public string BlogTitle { get; set; }
+	public string BlogAuthor { get; set; }
+	public string BlogContent {  get; set; }
+	public bool DeleteFlag {  get; set; }
 }
