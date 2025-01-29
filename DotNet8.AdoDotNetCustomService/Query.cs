9f6a896b1b6bec6fd062fc7f61423b515426bc97
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-29 22:15:56 +0630

    Feat : Add GetAllBlogsQuery in Query

--- DotNet8.AdoDotNetCustomService/Query.cs
+++ DotNet8.AdoDotNetCustomService/Query.cs
@@ -8,5 +8,12 @@ namespace DotNet8.AdoDotNetCustomService
 {
 	public class Query
 	{
+		public static string GetAllBlogsQuery { get; } =
+			@"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
 	}
 }
