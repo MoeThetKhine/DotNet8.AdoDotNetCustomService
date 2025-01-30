d3e22b7f716cd12028fa59b614a925c331771ae4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 14:28:51 +0630

    Feat : Add Create

--- DotNet8.AdoDotNetCustomService/Program.cs
+++ DotNet8.AdoDotNetCustomService/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace DotNet8.AdoDotNetCustomService;
+﻿using System.Data.SqlClient;
+
+namespace DotNet8.AdoDotNetCustomService;
 
 public class Program
 {
 	public static async Task Main(string[] args)
 	{
 		DotNetEnv.Env.Load(".env");
+
 		await Read();
+		//await Create("Sample Title", "Sample Author", "Sample Content");
 	}
 
 	public static async Task Read()
@@ -18,13 +22,36 @@ public class Program
 
 			foreach (var item in lst)
 			{
-				Console.WriteLine($"Blog Id : {item.BlogId}");
-				Console.WriteLine($"Blog Title : {item.BlogTitle}");
-				Console.WriteLine($"Blog Author : {item.BlogAuthor}");
-				Console.WriteLine($"Blog Content : {item.BlogContent}");
+				Console.WriteLine($"Blog Id: {item.BlogId}");
+				Console.WriteLine($"Blog Title: {item.BlogTitle}");
+				Console.WriteLine($"Blog Author: {item.BlogAuthor}");
+				Console.WriteLine($"Blog Content: {item.BlogContent}");
 			}
 		}
-		catch(CustomException ex)
+		catch (CustomException ex)
+		{
+			throw new CustomException(ex.Message);
+		}
+	}
+
+	public static async Task Create(string blogTitle, string blogAuthor, string blogContent)
+	{
+		try
+		{
+			AdoDotNetService adoDotNetService = new();
+			string query = Query.CreateBlogQuery;
+			List<SqlParameter> parameters =
+				new()
+				{
+					new("@BlogTitle", blogTitle),
+					new("@BlogAuthor", blogAuthor),
+					new("@BlogContent", blogContent)
+				};
+			int result = await adoDotNetService.ExecuteAsync(query, parameters.ToArray());
+
+			Console.WriteLine(result > 0 ? "Saving Successful." : "Saving Fail.");
+		}
+		catch (CustomException ex)
 		{
 			throw new CustomException(ex.Message);
 		}
