73644c8599fdaa0b44cf72bf729a340ef700f28a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 14:31:46 +0630

    -

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -5,11 +5,7 @@
 		public readonly string _connStr =
 			$"Data Source={Environment.GetEnvironmentVariable("DataSource")};Database={Environment.GetEnvironmentVariable("Database")};User ID={Environment.GetEnvironmentVariable("UserID")};Password={Environment.GetEnvironmentVariable("Password")};TrustServerCertificate=True;";
 
-		public async Task<List<T>> QueryAsync<T>(
-			string query,
-			SqlParameter[]? parameters = null,
-			SqlTransaction? transaction = null
-		)
+		public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
 		{
 			SqlConnection conn = GetConnection();
 			await conn.OpenAsync();
