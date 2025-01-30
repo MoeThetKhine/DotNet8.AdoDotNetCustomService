68c1e80a2dedea51d5a10010e3118705696fd387
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 16:29:24 +0630

    Feat : Update AdoDotNetService

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -1,73 +1,79 @@
-﻿namespace DotNet8.AdoDotNetCustomService;
-
-public class AdoDotNetService
+﻿namespace DotNet8.AdoDotNetCustomService
 {
-	public readonly string _connStr =
-		$"Data Source={Environment.GetEnvironmentVariable("DataSource")};Database={Environment.GetEnvironmentVariable("Database")};User ID={Environment.GetEnvironmentVariable("UserID")};Password={Environment.GetEnvironmentVariable("Password")};TrustServerCertificate=True;";
-
-	#region QueryAsync
-
-	public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+	public class AdoDotNetService
 	{
-		SqlConnection conn = GetConnection();
-		await conn.OpenAsync();
-		SqlCommand cmd = new(query, conn, transaction);
-		if (parameters is not null)
+		private readonly string _connStr = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+		#region QueryAsync
+
+		public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
 		{
-			cmd.Parameters.AddRange(parameters);
+			using SqlConnection conn = GetConnection();
+			await conn.OpenAsync();
+			using SqlCommand cmd = new(query, conn, transaction);
+
+			if (parameters is not null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+
+			SqlDataAdapter adapter = new(cmd);
+			DataTable dt = new();
+			adapter.Fill(dt);
+			await conn.CloseAsync();
+
+			string jsonStr = JsonConvert.SerializeObject(dt);
+			var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+			return lst;
 		}
-		SqlDataAdapter adapter = new(cmd);
-		DataTable dt = new();
-		adapter.Fill(dt);
-		await conn.CloseAsync();
 
-		string jsonStr = JsonConvert.SerializeObject(dt);
-		var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+		#endregion
 
-		return lst;
-	}
+		#region QueryFirstOrDefaultAsync
 
-	#endregion
-
-	#region QueryFirstOrDefaultAsync
-
-	public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
-	{
-		SqlConnection conn = GetConnection();
-		await conn.OpenAsync();
-		SqlCommand cmd = new(query, conn, transaction);
-		if (parameters is not null)
+		public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
 		{
-			cmd.Parameters.AddRange(parameters);
+			using SqlConnection conn = GetConnection();
+			await conn.OpenAsync();
+			using SqlCommand cmd = new(query, conn, transaction);
+
+			if (parameters is not null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+
+			SqlDataAdapter adapter = new(cmd);
+			DataTable dt = new();
+			adapter.Fill(dt);
+			await conn.CloseAsync();
+
+			return dt;
 		}
-		SqlDataAdapter adapter = new(cmd);
-		DataTable dt = new();
-		adapter.Fill(dt);
-		await conn.CloseAsync();
 
-		return dt;
-	}
+		#endregion
 
-	#endregion
+		#region ExecuteAsync
 
-	#region ExecuteAsync
-
-	public async Task<int> ExecuteAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
-	{
-		SqlConnection conn = GetConnection();
-		await conn.OpenAsync();
-		SqlCommand cmd = new(query, conn, transaction);
-		if (parameters is not null)
+		public async Task<int> ExecuteAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
 		{
-			cmd.Parameters.AddRange(parameters);
+			using SqlConnection conn = GetConnection();
+			await conn.OpenAsync();
+			using SqlCommand cmd = new(query, conn, transaction);
+
+			if (parameters is not null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+
+			int result = await cmd.ExecuteNonQueryAsync();
+			await conn.CloseAsync();
+
+			return result;
 		}
-		int result = await cmd.ExecuteNonQueryAsync();
-		await conn.CloseAsync();
 
-		return result;
+		#endregion
+
+		private SqlConnection GetConnection() => new(_connStr);
 	}
-
-	#endregion
-
-	private SqlConnection GetConnection() => new(_connStr);
 }
