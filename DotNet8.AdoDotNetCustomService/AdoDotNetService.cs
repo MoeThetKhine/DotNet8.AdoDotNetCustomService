9d65e72061815d82ca0b90d455e8968feb6210f5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 10:31:24 +0630

    -

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -1,11 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.AdoDotNetCustomService
 {
