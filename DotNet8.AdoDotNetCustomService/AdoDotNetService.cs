f4c80c94035d7e076e7515db487e5ef28c15fe6a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 10:06:07 +0630

    Feat : Add QueryAsync in Service

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNet8.AdoDotNetCustomService
 {
-	internal class AdoDotNetService
+	public class AdoDotNetService
 	{
+		public readonly string _connStr =
+		$"Data Source={Environment.GetEnvironmentVariable("DataSource")};Database={Environment.GetEnvironmentVariable("Database")};User ID={Environment.GetEnvironmentVariable("UserID")};Password={Environment.GetEnvironmentVariable("Password")};TrustServerCertificate=True;";
+
+		public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+		{
+			SqlConnection conn = GetConnection();
+			await conn.OpenAsync();
+			SqlCommand cmd = new(query, conn, transaction);
+
+			if(parameters is not null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+			SqlDataAdapter adapter = new(cmd);
+			DataTable dt = new();
+			adapter.Fill(dt);
+			await conn.CloseAsync();
+
+			string jsonStr = JsonConvert.SerializeObject(dt);
+			var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+			return lst;
+		}
+
+		private SqlConnection GetConnection() => new(_connStr);
 	}
+
+
 }
