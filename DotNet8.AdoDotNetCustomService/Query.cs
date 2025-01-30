74fdd44bf3a13b652c8afccc73d350611be99a2f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 16:30:31 +0630

    Feat : Add Query

--- DotNet8.AdoDotNetCustomService/Query.cs
+++ DotNet8.AdoDotNetCustomService/Query.cs
@@ -20,8 +20,7 @@ public class Query
 	#region CreateBlogQuery
 
 	public static string CreateBlogQuery { get; } =
-   @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent)
-VALUES(@BlogTitle, @BlogAuthor, @BlogContent)";
+  "INSERT INTO dbo.Tbl_Blog (BlogTitle, BlogAuthor, BlogContent, DeleteFlag) VALUES (@BlogTitle, @BlogAuthor, @BlogContent, @DeleteFlag);"; 
 
 	#endregion
 }
