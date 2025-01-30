95330f6342482e8308ce2e3ed145394468a08fa9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 10:46:17 +0630

    Feat : Add Read

--- DotNet8.AdoDotNetCustomService/Program.cs
+++ DotNet8.AdoDotNetCustomService/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DotNet8.AdoDotNetCustomService;
+
+public class Program
+{
+	public static async Task Main(string[] args)
+	{
+		DotNetEnv.Env.Load(".env");
+		await Read();
+	}
+
+	public static async Task Read()
+	{
+		try
+		{
+			AdoDotNetService adoDotNetService = new();
+			string query = Query.GetAllBlogsQuery;
+			List<BlogModel> lst = await adoDotNetService.QueryAsync<BlogModel>(query);
+
+			foreach (var item in lst)
+			{
+				Console.WriteLine($"Blog Id : {item.BlogId}");
+				Console.WriteLine($"Blog Title : {item.BlogTitle}");
+				Console.WriteLine($"Blog Author : {item.BlogAuthor}");
+				Console.WriteLine($"Blog Content : {item.BlogContent}");
+			}
+		}
+		catch(CustomException ex)
+		{
+			throw new CustomException(ex.Message);
+		}
+	}
+}
