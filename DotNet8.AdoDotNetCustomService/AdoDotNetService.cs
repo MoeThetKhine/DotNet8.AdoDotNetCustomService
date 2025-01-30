59c4ee899f7f15aa2bd67bfca9e41f27300eb3d4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 10:19:00 +0630

    Feat : Add QueryFirstOrDefaultAsync in Service

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -35,6 +35,24 @@ namespace DotNet8.AdoDotNetCustomService
 			return lst;
 		}
 
+		public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+		{
+			SqlConnection conn = GetConnection();
+			await conn.OpenAsync();
+			SqlCommand cmd = new(query, conn, transaction);
+			if(parameters is not null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+			SqlDataAdapter adapter = new(cmd);
+			DataTable dt = new();
+			adapter.Fill(dt);
+			await conn.CloseAsync();
+
+			return dt;
+
+		}
+
 		private SqlConnection GetConnection() => new(_connStr);
 	}
 
