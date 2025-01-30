1e3bdda439cbe217b321b89daee45c5465e91145
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 17:43:33 +0630

    Format Code

--- DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
+++ DotNet8.AdoDotNetCustomService/AdoDotNetService.cs
@@ -1,79 +1,78 @@
-﻿namespace DotNet8.AdoDotNetCustomService
+﻿namespace DotNet8.AdoDotNetCustomService;
+
+public class AdoDotNetService
 {
-	public class AdoDotNetService
+	private readonly string _connStr = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+	#region QueryAsync
+
+	public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
 	{
-		private readonly string _connStr = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
+		using SqlConnection conn = GetConnection();
+		await conn.OpenAsync();
+		using SqlCommand cmd = new(query, conn, transaction);
 
-		#region QueryAsync
-
-		public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+		if (parameters is not null)
 		{
-			using SqlConnection conn = GetConnection();
-			await conn.OpenAsync();
-			using SqlCommand cmd = new(query, conn, transaction);
-
-			if (parameters is not null)
-			{
-				cmd.Parameters.AddRange(parameters);
-			}
-
-			SqlDataAdapter adapter = new(cmd);
-			DataTable dt = new();
-			adapter.Fill(dt);
-			await conn.CloseAsync();
-
-			string jsonStr = JsonConvert.SerializeObject(dt);
-			var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
-
-			return lst;
+			cmd.Parameters.AddRange(parameters);
 		}
 
-		#endregion
+		SqlDataAdapter adapter = new(cmd);
+		DataTable dt = new();
+		adapter.Fill(dt);
+		await conn.CloseAsync();
 
-		#region QueryFirstOrDefaultAsync
+		string jsonStr = JsonConvert.SerializeObject(dt);
+		var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
 
-		public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
-		{
-			using SqlConnection conn = GetConnection();
-			await conn.OpenAsync();
-			using SqlCommand cmd = new(query, conn, transaction);
-
-			if (parameters is not null)
-			{
-				cmd.Parameters.AddRange(parameters);
-			}
-
-			SqlDataAdapter adapter = new(cmd);
-			DataTable dt = new();
-			adapter.Fill(dt);
-			await conn.CloseAsync();
-
-			return dt;
-		}
-
-		#endregion
-
-		#region ExecuteAsync
-
-		public async Task<int> ExecuteAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
-		{
-			using SqlConnection conn = GetConnection();
-			await conn.OpenAsync();
-			using SqlCommand cmd = new(query, conn, transaction);
-
-			if (parameters is not null)
-			{
-				cmd.Parameters.AddRange(parameters);
-			}
-
-			int result = await cmd.ExecuteNonQueryAsync();
-			await conn.CloseAsync();
-
-			return result;
-		}
-
-		#endregion
-
-		private SqlConnection GetConnection() => new(_connStr);
+		return lst;
 	}
+
+	#endregion
+
+	#region QueryFirstOrDefaultAsync
+
+	public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+	{
+		using SqlConnection conn = GetConnection();
+		await conn.OpenAsync();
+		using SqlCommand cmd = new(query, conn, transaction);
+
+		if (parameters is not null)
+		{
+			cmd.Parameters.AddRange(parameters);
+		}
+
+		SqlDataAdapter adapter = new(cmd);
+		DataTable dt = new();
+		adapter.Fill(dt);
+		await conn.CloseAsync();
+
+		return dt;
+	}
+
+	#endregion
+
+	#region ExecuteAsync
+
+	public async Task<int> ExecuteAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+	{
+		using SqlConnection conn = GetConnection();
+		await conn.OpenAsync();
+		using SqlCommand cmd = new(query, conn, transaction);
+
+		if (parameters is not null)
+		{
+			cmd.Parameters.AddRange(parameters);
+		}
+
+		int result = await cmd.ExecuteNonQueryAsync();
+		await conn.CloseAsync();
+
+		return result;
+	}
+
+	#endregion
+
+	private SqlConnection GetConnection() => new(_connStr);
 }
